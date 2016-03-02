d1d539ace25af16f5ab78e324b70aa6b837f01ab
tcnksm <nsd22843@gmail.com>
2016-03-02 21:40:59 +0900

    Allow to output to Discard

--- docker.go
+++ docker.go
@@ -1,15 +1,26 @@
 package main
 
 import (
-	"os"
+	"io"
+	"io/ioutil"
 	"os/exec"
 )
 
+type Docker struct {
+	OutStream io.Writer
+	Discard   bool
+}
+
 // docker runs docker command
-func docker(args ...string) error {
+func (d *Docker) execute(args ...string) error {
 	cmd := exec.Command("docker", args...)
-	cmd.Stderr = os.Stdout // ioutil.Discard
-	cmd.Stdout = os.Stdout // ioutil.Discard
+	cmd.Stderr = d.OutStream
+	cmd.Stdout = d.OutStream
+
+	if d.Discard {
+		cmd.Stderr = ioutil.Discard
+		cmd.Stdout = ioutil.Discard
+	}
 
 	if err := cmd.Run(); err != nil {
 		return err
